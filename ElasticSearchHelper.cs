d3b64166118ef60981fdcbac578cac977ee783fe
Samuel Martinez <samuel.martinez192@outlook.com>
2020-05-18 15:29:16 -0600

    Add project files.

---
+++ ElasticSearchHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAdvert.SearchWorker
+{
+    public static class ElasticSearchHelper
+    {
+        private static IElasticClient _client;
+        public static IElasticClient GetInstance(IConfiguration config)
+        {
+            if (_client == null)
+            {
+                var url = config.GetSection("ES").GetValue<string>("url");
+                var settings = new ConnectionSettings(new Uri(url)).DefaultIndex("advert")
+                    .DefaultMappingFor<AdvertType>(m => m.IdProperty(x => x.Id));
+                
+                _client = new ElasticClient(settings);
+            }
+
+            return _client;
+        }
+    }
+}
---
+++ MappingHelper.cs
@@ -0,0 +1,22 @@
+﻿using AdvertApi.Models.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAdvert.SearchWorker
+{
+    public class MappingHelper
+    {
+        public static AdvertType Map(AdvertConfirmedMessage message)
+        {
+            var doc = new AdvertType
+            {
+                Id = message.Id,
+                Title = message.Title,
+                CreationDateTime = DateTime.Now
+            };
+
+            return doc;
+        }
+    }
+}
